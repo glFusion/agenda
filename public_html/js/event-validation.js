204db34d96bafd42cb4cde10f17bababced19c45
Mark Evans <mark@glfusion.org>
2017-11-10 12:27:37 -0600

    Add end date validation when all days is checked.

--- public_html/js/event-validation.js
+++ public_html/js/event-validation.js
@@ -15,6 +15,23 @@
 $(document).ready(function () {
 
 	$.validator.addMethod("enddate", function(value, element) {
+		if ($('#event-allday').is(':checked') == false) {
+			return true;
+		}
+		var from_time = $("#start-time").val();
+		var to_time 	= $("#end-time").val();
+		var start_date = $("#event-date").val();
+		var end_date 	= $("#event-end-date").val();
+		var momentFrom = moment(start_date + ' ' + from_time, 'YYYY-MM-DD hh:mm A');
+		var momentTo   = moment(end_date + ' ' + to_time, 'YYYY-MM-DD hh:mm A');
+		if (momentFrom > momentTo) {
+			return false;
+		} else {
+			return true;
+		}
+	}, " * " + lang['err_end_before_start']);
+
+	$.validator.addMethod("eventend", function(value, element) {
 		var from_time = $("#start-time").val();
 		var to_time 	= $("#end-time").val();
 		var start_date = $("#event-date").val();
@@ -47,19 +64,19 @@ $(document).ready(function () {
 
 	$( "#event-form" ).validate({
 	  focusCleanup: true,
-//	  onfocusout: true,
 		errorElement: 'span',
 		errorClass: 'uk-text-danger uk-text-bold',
 		rules: {
 			"title": { required:true },
-			"end-time": { enddate: true },
+			"end-time": { eventend: true },
+			"event-end-date": { enddate: true },
 			"event-allday": { allday: true },
 		},
 		messages: {
 			"title":{	required: ' * ' + lang['err_enter_title']	},
 		},
 		errorPlacement: function(error, element) {
-			if (element.attr("name") == "end-time" || element.attr("name") == "event-allday" ) {
+			if (element.attr("name") == "end-time" || element.attr("name") == "event-allday" || element.attr("name") == 'event-end-date' ) {
 				error.insertAfter('#date-errors');
 			} else {
 				error.insertAfter( element );
